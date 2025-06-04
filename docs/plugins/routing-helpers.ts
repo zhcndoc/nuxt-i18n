ef6b7faa1127fbbb3cbc41a4d7efed6e04f5d448
一纸忘忧 <i@ikxin.com>
2025-06-04 09:33:02 +0800

    refactor: remove outdated routing logic from routing-helpers

--- docs/plugins/routing-helpers.ts
+++ docs/plugins/routing-helpers.ts
@@ -2,22 +2,6 @@ import { mapContentNavigation } from '@nuxt/ui-pro/runtime/utils/content.js'
 import type { ContentNavigationItem } from '@nuxt/content'
 
 export default defineNuxtPlugin(async () => {
-<<<<<<< HEAD
-  const router = useRouter()
-
-  if (import.meta.server) {
-    const req = useRequestURL()
-    if (req.hostname.startsWith('v8') && req.pathname === '/') {
-      navigateTo('/docs/v8/getting-started')
-    }
-
-    if (req.hostname.startsWith('v7') && req.pathname === '/') {
-      navigateTo('/docs/v7/setup')
-    }
-  }
-
-=======
->>>>>>> origin/upstream
   const { data: navigation } = await useAsyncData('navigation', () => queryCollectionNavigation('docs'))
   const nav = computed<ContentNavigationItem[]>(
     () => mapContentNavigation(navigation.value).at(0).children as ContentNavigationItem[]
